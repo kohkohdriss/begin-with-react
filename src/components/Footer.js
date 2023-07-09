589b29e054183d599f7e4a9c54ac87d9a33c4ecc
Driss KOHKOH <kohkoh.driss@gmail.com>
2023-07-09 20:38:42 +0200

    ajout des composant, utilisation de lists, props et events

---
+++ src/components/Footer.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import "../styles/Footer.css";
+
+function Footer() {
+  const [inputValue, setInputValue] = useState("");
+  console.log("inputValue", inputValue);
+  function handleInput(e) {
+    setInputValue(e.target.value);
+  }
+
+  function handleBlur() {
+    if (!inputValue.includes("@")) {
+      alert(
+        "Attention, il n'y a pas d'@, ceci n'est pas une adresse valide 😥"
+      );
+    } else {
+      alert("c'est ok");
+    }
+  }
+
+  return (
+    <footer className="lmj-footer">
+      <div className="lmj-footer-elem">
+        Pour les passionné·e·s de plantes 🌿🌱🌵
+      </div>
+      <div className="lmj-footer-elem">Laissez-nous votre mail :</div>
+      <input
+        placeholder="Entrez votre mail"
+        onChange={handleInput}
+        value={inputValue}
+        // defaultValue={inputValue}
+        onBlur={handleBlur}
+      />
+    </footer>
+  );
+}
+
+export default Footer;
